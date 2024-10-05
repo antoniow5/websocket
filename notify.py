1e9cfc5fa849d6060eac933915f73a5ac80834a1
antoniow <cool.antoniow@gmail.com>
2024-10-05 18:02:57 +0900

    added readme

--- notify.py
+++ notify.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, HTTPException, Request, Query
 from fastapi.responses import JSONResponse, HTMLResponse
+from dotenv import load_dotenv
 import requests
 import hashlib
+import os
 
 
 app = FastAPI()
 
-AUTH_URL = "https://devapi.edunity.kz/ru/api/v1/webhook/auth"
-CLIENT_ID = '8b331c89f0'
-CLIENT_SECRET = '583988d21b04aad9bee7963a566f8f32'
+load_dotenv()
+
+AUTH_URL = os.getenv('AUTH_URL')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+TESTING = os.getenv('TESTING', 'False').lower() == 'true'
 
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections = {}  # user_id -> list of websockets
+        self.active_connections = {}  
 
     async def connect(self, websocket: WebSocket, user_id: int):
         if str(user_id) not in self.active_connections:
@@ -49,36 +54,29 @@ async def websocket_endpoint(websocket: WebSocket, cid: str = Query(...)):
     user_id = None
 
     try:
-        # Receive the first message to get the token
         data = await websocket.receive_json()
         token = data.get('token')
-
-        # Authenticate with external API
         headers = {"Authorization": f"Bearer {token}"}
         response = requests.get(f"{AUTH_URL}?cid={cid}", headers=headers)
 
         if response.status_code == 200:
-            # Get user_id from the response
             user_id = response.json()['data'].get("uid")
             if not user_id:
                 await websocket.close()
                 return
             
             try:
-                user_id = int(user_id)  # Ensure it's a valid integer
+                user_id = int(user_id) 
             except ValueError:
                 print('Invalid user_id, closing websocket')
-                await websocket.close(code=1003)  # 1003: Unsupported Data
+                await websocket.close(code=1003) 
                 return
 
-            # Connect the user
             await manager.connect(websocket, user_id)
 
-            # Main loop to keep connection alive and listen for messages
             try:
                 while True:
-                    data = await websocket.receive_json()  # Listening for incoming messages
-                    # Handle received messages here (if needed)
+                    data = await websocket.receive_json() 
 
             except WebSocketDisconnect:
                 print('WebSocket disconnected')
@@ -86,7 +84,7 @@ async def websocket_endpoint(websocket: WebSocket, cid: str = Query(...)):
 
         else:
             print('Authentication failed, closing WebSocket')
-            await websocket.close(code=1008)  # 1008: Policy Violation (invalid token)
+            await websocket.close(code=1008)  
 
     except WebSocketDisconnect:
         print('WebSocket closed by client')
@@ -97,9 +95,8 @@ async def websocket_endpoint(websocket: WebSocket, cid: str = Query(...)):
 
     except Exception as e:
         print(f'Error occurred: {e}')
-        # Handle other exceptions that might occur
         await websocket.close(code=1011)
-        return  # 1011: Internal Server Error
+        return  
 
 
 
@@ -139,7 +136,6 @@ async def notify(request: Request):
         user_id = item.get("user_id")
         body = item.get("body")
 
-        # Validate the presence of user_id and message
         if not user_id or not body or type(body) != dict or type(user_id) != int:
             results.append({"user_id": user_id, "status": "failed", "error": "user_id and body are required"})
             continue
@@ -160,6 +156,9 @@ async def notify(request: Request):
 
 @app.get("/test", response_class=HTMLResponse)
 async def get_html():
+    if not TESTING:
+        raise HTTPException(status_code=403, detail="Testing mode is disabled")
+    
     html_content = """
     <!DOCTYPE html>
     <html>
@@ -189,7 +188,7 @@ async def get_html():
             function connectWebSocket() {
                 const token = document.getElementById('token').value;
                 const cid = document.getElementById('cid').value;
-                websocket = new WebSocket(`wss://tkanini.kz/ws?cid=${cid}`);
+                websocket = new WebSocket(`wss://${window.location.host}/ws?cid=${cid}`);
 
                 websocket.onopen = function(event) {
                     document.getElementById('status').innerText = 'Connected';
